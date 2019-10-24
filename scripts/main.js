a4370e36b699d0cc0692db8a0385df9f82a5b773
jonat2021888 <56306763+jonat2021888@users.noreply.github.com>
2019-10-24 14:13:01 -0400

    My magic 8 ball
    Last minute changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 
 let happyCore = {
-  results: ['Im exited', 'Super yes!!', 'Awesome']
+  results: ['Im exited', 'Super yes!!', 'Awesome', "cool", "You Rock"]
 }
 let sadCore = {
-  results:['No', 'Horrible', 'More or less']
+  results:['No', 'Horrible', 'More or less', "Im destroyed in the inside"]
 }
 
 let meanCore = {
-  results: ['I hate you', 'Disgusting', 'Stupid']
+  results: ['I hate you', 'Disgusting', 'Stupid', "Go away", "Leave me alone"]
 }
 
 let sillyCore = {
-  results: ["oops i did it again", "Eww you stink", "I like turtles"]
+  results: ["oops i did it again", "Eww you stink", "I like turtles", "You look like my mink"]
 
 }
 
@@ -20,7 +20,7 @@ let core = happyCore
 //  Magic 8-ball in one line
 function shake() {
   console.log(document.getElementById("mood").value);
-  document.getElementById("outp").innerHTML = core.results[Math.floor(Math.random() *3)]
+  document.getElementById("outp").innerHTML = core.results[Math.floor(Math.random() *4)]
   if (document.getElementById("mood").value == "Happy") {
     core = happyCore
   }
